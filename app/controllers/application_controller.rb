8656d9e2149b577c894442ca10ae11a807bbce0f
Oleg Bulkin <o.bulkin@gmail.com>
2016-03-31 22:35:07 +0000

    Configured the app to store the previous non-Devise URL as a user browses around and redirect to it after a sign in or sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :store_location
+
+  # Store the previous URL as a user browses around and redirect to it after a sign in or sign up
+  private
+  def store_location
+    session[:previous_path] = request.fullpath unless request.fullpath.include?("/users") || !request.get?
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    session[:previous_path] || root_path
+  end
+
+  def after_sign_up_path_for(resource_or_scope)
+    session[:previous_path] || root_path
+  end
 end
